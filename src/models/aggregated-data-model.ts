b0253e82cd6ff2d5ed4871c639bcfc90e359bd72
slavchetrenkovski <slavche@byondxr.com>
2024-07-06 00:50:05 +0200

    base interface and extending

--- src/models/aggregated-data-model.ts
+++ src/models/aggregated-data-model.ts
@@ -1,17 +1,15 @@
-export interface PackageVulnerability {
-  packageName: string;
+export interface VulnerabilityCounts {
   critical: number;
   high: number;
   medium: number;
   low: number;
 }
 
-export interface TotalVulnerabilitiesBySeverity {
-  critical: number;
-  high: number;
-  medium: number;
-  low: number;
+export interface PackageVulnerability extends VulnerabilityCounts {
+  packageName: string;
 }
+
+export interface TotalVulnerabilitiesBySeverity extends VulnerabilityCounts {}
 
 export interface CompanyVulnerability {
   createdAt: string;
